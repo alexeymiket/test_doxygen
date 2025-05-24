f432b5bc31a7ab085d62b2351a6942d6b8e4848a
Алексей Микет <alexeymiket@MacBook-Air-M2.local>
2025-05-24 10:56:16 +1000

    commit 2

--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['main_0',['main',['../main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.cpp']]]
+  ['main_0',['main',['../main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.cpp']]],
+  ['mean_1',['mean',['../class_statistics.html#aaf07e2fe2a9f1ee3331f7af016c16c22',1,'Statistics']]]
 ];
